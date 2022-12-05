21d7c29d7389ba1fbdc2f6fa802d850b49afcd5b
rinaldi600 <rinaldih84@gmail.com>
2022-12-05 15:44:44 +0700

    Code UI UX home 7

---
+++ whatsapp-app/resources/js/Pages/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className={"mobile:w-full sm:w-[193px] h-[200px] bg-white shadow-lg rounded-lg absolute mobile:top-14 sm:top-20 md:top-16 xl:top-12 mobile:right-0 sm:right-2"}>
+            <h1>WORK</h1>
+        </div>
+    )
+}
+
+export default Navbar;
