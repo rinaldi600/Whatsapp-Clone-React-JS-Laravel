d8c45e4d19910c3d7e916aa34185d5dc6889bbc7
rinaldi600 <rinaldih84@gmail.com>
2022-12-12 00:32:55 +0700

    Code Ui UX home 10

---
+++ whatsapp-app/resources/js/Pages/ProfileChatUser/ProfileChatUser.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function ProfileChatUser({profile, name}) {
+    return (
+        <div className={"flex hover:bg-[#F5F6F6] hover:rounded-[5px] hover:p-2 gap-3 items-center cursor-pointer bg-white"}>
+            <div className={"w-[45px] h-[45px] overflow-hidden rounded-full bg-yellow-400"}>
+                <img className={"w-full h-full"} src={profile} alt=""/>
+            </div>
+            <div className={"flex justify-between w-[80%] border-b-[0.5px] border-[#E9EDEF]"}>
+                <div className={"mb-1"}>
+                    <p className={"font-medium text-[#111b21] text-lg"}>{name}</p>
+                    <p className={"text-[#3b4a54] text-sm"}>Oke berangkat...</p>
+                </div>
+                <div>
+                    <p className={"text-[#1FA855] text-xs font-semibold"}>19.40</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileChatUser;
